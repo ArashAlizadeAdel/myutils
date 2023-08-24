caba7c6feff535e6c13332037cf141ff409a2400
ArashAlizadeAdel <arashalizadeadel@gmail.com>
2023-08-24 17:03:12 +0330

    fixing a typo

--- core/design_patterns/prototype.py
+++ core/design_patterns/prototype.py
@@ -5,7 +5,7 @@ class Prototype(ABC):
     '''the prototype object'''
     
     def clone(self):
-        '''The abstract method that enables prototyping. It must be implemented by
+        '''The method that enables prototyping. It must be implemented by
         the subclass. use super() to implement the default implementation.
         '''
         return copy.deepcopy(self)
